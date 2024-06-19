2dc9e866f97e2341304292ff7466a4a9e8d9b1de
GhazanfarShahbaz <ghazanfar.shahbaz95@myhunter.cuny.edu>
2024-06-19 18:46:50 -0400

    TEST

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,2 +1,6 @@
-def test_dummy() -> None:
-    assert 1 == 1
+from token_granter_wrapper import token_granter
+
+
+def test_token_granter():
+    tk = token_granter.TokenGranter("here")
+    assert "" == tk.grant_access_token("TEST", "TEST", False)
